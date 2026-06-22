0d584c562d940cc357b04e52c5b1f4e2feb191cf
pablormacia <pr.macia@gmail.com>
2026-06-22 15:35:34 -0300

    Mejora de formulario

--- core/models.py
+++ core/models.py
@@ -22,7 +22,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField()
+    published_date = models.DateTimeField(auto_now_add=True)
 
     author = models.ForeignKey(
         Author,
