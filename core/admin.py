adbc3ebc2b64709621cbb29cf92bb873259fd22c
pablormacia <pr.macia@gmail.com>
2026-06-23 10:05:29 -0300

    Agregada home como inicio

--- core/admin.py
+++ core/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from .models import Author, Post, Tag
 
 
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 1
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("name", "email")
     search_fields = ("name", "email")
+    inlines = [PostInline]
+
 
 
 @admin.register(Post)
@@ -25,9 +32,11 @@ class PostAdmin(admin.ModelAdmin):
         "author",
         "published_date",
     )
+    filter_horizontal = ("tags",)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
+
